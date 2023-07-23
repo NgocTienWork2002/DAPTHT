1b477e67c85842412e66fb15420336fff90435f1
NgocTienWork2002 <nguyenngoctien.chatgpt@gmail.com>
2023-07-23 13:55:45 +0700

    Giao diện đăng nhập

--- public/java/login.js
+++ public/java/login.js
@@ -1,33 +1,17 @@
-function btnCancel(){
-    openPage('Home')
+function btnCancel() {
+  openPage("Home");
 }
 // Đăng ký ................
 
-$(document).ready(()=>{
-  document.getElementById('Home').style.display = "block";
-})
-// Bật 2 tab .............
-function openPage(pageName) {
-  if (pageName == 'Home'){
-    document.getElementById(pageName).style.display = "block";
-    document.getElementById('News').style.display = "none";
+$(document).ready(() => {
+  document.getElementById("Home").style.display = "block";
+});
+function Tab(evt, cityName) {
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tabLogin");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  else if(pageName == 'News'){
-    document.getElementById(pageName).style.display = "block";
-    document.getElementById('Home').style.display = "none";
-  }
-}
-// Đăng nhập ............
-function login(){
-  var User = $('.User').val()
-  if(User == 'Khang123' & $('#Role').val() == 'Cus'){
-    setTimeout(window.location.replace('../html/index.html'),4000)
-  }
-  else{
-    window.location.replace('../html/manage.html')
-  }
-}
-function SigUp(){
-  swal("success!","Đăng Kí Thành Công");
-  openPage('Home')
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
