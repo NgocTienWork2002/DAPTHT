42322355089fb6ec0f30ceb2df2a6cc136df6aaf
Nguyen Thuan Nam <nguyennamlda@gmail.com>
2023-07-30 22:45:17 +0700

    update 30/7
    Thêm chức năng đặt lịch

--- routes/booking.route.js
+++ routes/booking.route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 var serviceController = require("../controller/serviceController");
 
 router.get("/", serviceController.getBookingPage);
+router.get("/service", serviceController.getService);
+router.post("/", serviceController.bookingService);
 
 module.exports = router;
